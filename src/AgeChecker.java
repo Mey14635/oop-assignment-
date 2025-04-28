fac00fe2a02366564184afee36bf9e2954c8eb80
Mey14635 <kasomediatrice1407@gmail.com>
2025-04-28 22:24:31 +0300

    commit 3

---
+++ src/AgeChecker.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class AgeChecker {
+        Scanner scanner=new Scanner(System.in);
+    public void readNameAndAge() {
+        String name;
+        int age;
+        System.out.print("Name? :");
+        name=scanner.next();
+        System.out.print("Age? :");
+        age=scanner.nextInt();
+        System.out.println("Hello "+name+",You are "+age+ " springs young");
+    }
+    public static void main(String[] args)
+    {
+        (new AgeChecker()).readNameAndAge();
+    }
+}
